93b71f66e2f3a8624f793c676c7331743aed1c19
Tobias Bocanegra <tripodsan@users.noreply.github.com>
2020-01-23 08:59:36 +0900

    fix(core): provide initial version (#1)

--- README.md
+++ README.md
@@ -10,32 +10,36 @@
 [![LGTM Code Quality Grade: JavaScript](https://img.shields.io/lgtm/grade/javascript/g/adobe/helix-epsagon.svg?logo=lgtm&logoWidth=18)](https://lgtm.com/projects/g/adobe/helix-epsagon)
 [![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)
 
-## Installation
+# API Reference
+<a name="module_epsagon"></a>
 
-```bash
-$ npm install @adobe/helix-epsagon
+## epsagon ⇒ <code>ActionFunction</code>
+Wrap function that returns an OpenWhisk function is automatically instrumented with epsagon,
+if the `EPSAGON_TOKEN` action parameter is present.
+
+**Usage:**
+
+```js
+const { wrap } = require('@adobe/openwhisk-action-utils');
+const { epsagon } = require('@adobe/helix-epsagon');
+
+async function main(params) {
+  //…my action code…
+}
+
+module.exports.main = wrap(main)
+  .with(epsagon);
 ```
 
-## Usage
+**Returns**: <code>ActionFunction</code> - a new function with the same signature as your original main function  
 
-See the [API documentation](docs/API.md).
+| Param | Type | Default | Description |
+| --- | --- | --- | --- |
+| action | <code>ActionFunction</code> |  | Original OpenWhisk action main function |
+| [opts] | <code>EpsagonOptions</code> |  | Additional epsagon options |
+| [opts.sendTimeout] | <code>number</code> | <code>2</code> | Time in seconds after which the request to the epsagon infrastructure times out. |
+| [opts.token_param] | <code>string</code> | <code>&quot;EPSAGON_TOKEN&quot;</code> | The name of the action parameter that contains the epsagon token. |
+| [opts.appName] | <code>string</code> | <code>&quot;Helix Service&quot;</code> | The name of _this_ application. |
+| [opts.ignoredKeys] | <code>Array.&lt;RegExp, string&gt;</code> | <code>[/[A-Z0-9_]+/]</code> | Array of patterns for parameter keys to ignore in traces. |
+| [opts.urlPatternsToIgnore] | <code>Array.&lt;RegExp, string&gt;</code> | <code>[&#x27;api.coralogix.com&#x27;]</code> | Array of patterns for urls to ignore in traces. |
 
-## Development
-
-### Build
-
-```bash
-$ npm install
-```
-
-### Test
-
-```bash
-$ npm test
-```
-
-### Lint
-
-```bash
-$ npm run lint
-```
---
+++ src/epsagon.d.ts
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2020 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+/**
+ * The `main` function of an OpenWhisk action.
+ * @param {object} params the parameters of the action function
+ * @returns {object} a result
+ */
+declare type ActionFunction = (params: object) => object;
+
+/**
+ * Options for the epsagon wrap function
+ */
+export declare interface EpsagonOptions {
+  /**
+   * Timeout in seconds after which the request to the epsagon infrastructure times out.
+   * @default 2
+   */
+  sendTimeout?: number,
+
+  /**
+   * The name of the action parameter that contains the epsagon token.
+   * @default 'EPSAGON_TOKEN'
+   */
+  token_param?: string,
+
+  /**
+   * The name of _this_ application
+   * @default 'Helix Services'
+   */
+  appName?: string,
+
+  /**
+   * Array of patterns for parameter keys to ignore in traces.
+   * @default [/[A-Z0-9_]+/]
+   */
+  ignoredKeys?: Array<RegExp|string>,
+
+  /**
+   * Array of patterns for urls to ignore in traces.
+   * @default ['api.coralogix.com']
+   */
+  urlPatternsToIgnore?: Array<RegExp|string>,
+}
+
+/**
+ * Wrap function that returns an OpenWhisk function is automatically instrumented with epsagon,
+ * if the `EPSAGON_TOKEN` action parameter is present.
+ *
+ * **Usage:**
+ *
+ * ```js
+ * const { wrap } = require('@adobe/openwhisk-action-utils');
+ * const { epsagon } = require('@adobe/helix-epsagon');
+ *
+ * async function main(params) {
+ *   //…my action code…
+ * }
+ *
+ * module.exports.main = wrap(main)
+ *   .with(epsagon);
+ * ```
+ *
+ * @function logger
+ * @param {ActionFunction} fn - original OpenWhisk action main function
+ * @param {EpsagonOptions} [opts] - optional options.
+ * @returns {ActionFunction} a new function with the same signature as your original main function
+ */
+export declare function epsagon(fn: ActionFunction, opts: EpsagonOptions): ActionFunction;
